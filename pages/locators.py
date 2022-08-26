c150974329507a58352d47ff0203add528bb9356
Evgeny Shamray <EvSha83@yandex.ru>
2022-08-26 16:32:49 +0300

    "Add login and registration form tests. Add locators forms in file "

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,10 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+class LoginPageLocators:
+    LOGIN_FORM_EMAIL = (By.CSS_SELECTOR,"input[name='login-username']")
+    LOGIN_FORM_PASSWORD =(By.CSS_SELECTOR,"input[name='login-password']")
+    REGISTR_FORM_EMAIL =(By.CSS_SELECTOR,"input[name='registration-email']")
+    REGISTR_FORM_PASSWORD =(By.CSS_SELECTOR,"input[name='registration-password1']")
+    REGISTR_FORM_PASSWORD2 =(By.CSS_SELECTOR,"input[name='registration-password2']")
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -18,3 +19,33 @@ def test_guest_should_see_login_link(browser):
     sleep(2)
     page_main.should_be_login_link()
     sleep(2)
+
+def test_word_login_in_url_login_page(browser):
+    url='http://selenium1py.pythonanywhere.com/'
+    page_main = MainPage(browser, url)
+    login_page = LoginPage(browser, url)
+    page_main.open_site()
+    sleep(2)
+    page_main.go_to_login_link()
+    sleep(2)
+    login_page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    url = 'http://selenium1py.pythonanywhere.com/'
+    page_main = MainPage(browser, url)
+    login_page = LoginPage(browser, url)
+    page_main.open_site()
+    sleep(2)
+    page_main.go_to_login_link()
+    sleep(2)
+    login_page.should_be_login_form()
+
+def test_guest_should_see_registr_form(browser):
+    url = 'http://selenium1py.pythonanywhere.com/'
+    page_main = MainPage(browser, url)
+    login_page = LoginPage(browser, url)
+    page_main.open_site()
+    sleep(2)
+    page_main.go_to_login_link()
+    sleep(2)
+    login_page.should_be_register_form()
